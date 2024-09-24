1d582f1687b1e5dc67b62521c004afbdcc372794
QUEVIN ORDONEZ <kevin4965@gmail.com>
2024-09-23 20:33:48 -0500

    Actualizando submódulo peliculas

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from 'react';
-import {DirectoresView} from './components/directores/DirectoresView';
+import DirectoresView from './components/directores/DirectoresView'; // Sin destructuración
 import GenerosView from './components/generos/GenerosView';
 import MediaView from './components/media/MediaView';
 import ProductorasView from './components/productoras/ProductorasView';
 import TiposView from './components/tipos/TiposView';
 import Header from './components/ui/Header';
-
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hola mundo</h1>
-    </div>
-  )
-}
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={GenerosView} />  {/* Ruta principal */}
+        <Route path="/generos" component={GenerosView} />
+        <Route path="/directores" component={DirectoresView} />
+        <Route path="/media" component={MediaView} />
+        <Route path="/productoras" component={ProductorasView} />
+        <Route path="/tipos" component={TiposView} />
+        <Redirect to="/" /> {/* Redirigir a la raíz si no coincide con ninguna ruta */}
+      </Switch>
+    </Router>
+  );
+};
 
-export default App
-
+export default App;
--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Importa Link
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <Link className="navbar-brand" to="/">Peliculas</Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -19,63 +20,31 @@ const Header = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
+              <Link className="nav-link" activeClassName='active' to="/generos">
+                Genero
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Dropdown
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Action
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Another action
-                  </a>
-                </li>
-                <li>
-                  <hr className="dropdown-divider" />
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Something else here
-                  </a>
-                </li>
-              </ul>
+              <Link className="nav-link" activeClassName='active' to="/directores">
+                Director
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled" aria-disabled="true">
-                Disabled
-              </a>
+              <Link className="nav-link" activeClassName='active' to="/productoras">
+                Productora
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" activeClassName='active' to="/tipos">
+                Tipo
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" activeClassName='active' to="/media">
+                Media
+              </Link>
             </li>
           </ul>
-          <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-            <button className="btn btn-outline-success" type="submit">
-              Search
-            </button>
-          </form>
         </div>
       </div>
     </nav>
@@ -83,3 +52,4 @@ const Header = () => {
 };
 
 export default Header;
+
